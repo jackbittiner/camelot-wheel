689afea115e38bf7cd6ecde66681d0c97bf76578
Jack Bittiner <jackbittiner@hotmail.com>
2019-11-26 16:45:56 +0000

    :bug: remove function invokers in module exports

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const getHarmonicKeys = require("./src/get-harmonic-keys");
 const getCamelotRoute = require("./src/get-camelot-route");
 
 module.exports = {
-  getKeyByPitchClassAndMode: getKey.getKeyByPitchClassAndMode(),
-  getKeyByCamelotPositionAndMode: getKey.getKeyByCamelotPositionAndMode(),
+  getKeyByPitchClassAndMode: getKey.getKeyByPitchClassAndMode,
+  getKeyByCamelotPositionAndMode: getKey.getKeyByCamelotPositionAndMode,
   getHarmonicKeys: getHarmonicKeys.getHarmonicKeys,
   getCamelotRoute: getCamelotRoute.getCamelotRoute
 };
