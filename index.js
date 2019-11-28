eb5347f7f132f17edff12c478ca0be50198a3772
Jack Bittiner <jackbittiner@hotmail.com>
2019-11-28 18:01:59 +0000

    :pencil: update variable name

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const keyModule = require("./src/get-key");
 const harmonicKeysModule = require("./src/get-harmonic-keys");
-const getCamelotRoute = require("./src/get-camelot-route");
+const camelotRouteModule = require("./src/get-camelot-route");
 
 module.exports = {
   getKey: keyModule.getKey,
   getHarmonicKeys: harmonicKeysModule.getHarmonicKeys,
-  getCamelotRoute: getCamelotRoute.getCamelotRoute
+  getCamelotRoute: camelotRouteModule.getCamelotRoute
 };
